8b609a4e87bc5f20e54809f85a87f54c7e048223
Sabiiii <sabinaajanovic27@gmail.com>
2021-09-17 00:24:17 +0400

    Adding Declare string variables

---
+++ ex10/src/index.js
@@ -0,0 +1,18 @@
+function main() {
+
+    var myFirstName = "Sabina";
+    var myLastName = "Ajanovic";
+    var myHomeTown = "Tuzla";
+
+
+    return {
+
+        myFirstName,
+        myLastName,
+        myHomeTown
+    };
+}
+
+console.log(main());
+
+module.exports = main;
